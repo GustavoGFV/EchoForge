839504509757daded7fb4d66a3af58b14352f64b
Gustavo Vieira <gustavofelipgfv@gmail.com>
2024-11-21 17:22:51 -0300

    WorkInProgress - PlayerAnimation

--- Scenes/Lvl1/player/player.gd
+++ Scenes/Lvl1/player/player.gd
@@ -27,6 +27,9 @@ func _init():
 		cooldown = 0.1
 		damage = 20
 
+@onready var textureAnimation:= $PlayerShape/Anim as AnimatedSprite2D
+@onready var texture := $PlayerShape/PlayerSprite as Sprite2D
+
 func _physics_process(delta):
 	#Handle Gravity
 	if !is_on_floor():
@@ -39,6 +42,23 @@ func _physics_process(delta):
 		
 	# Handle Horizontal Movement	
 	var direction = Input.get_axis("Left", "Right")
-	velocity.x = direction * speed
+	
+	if direction != 0:
+		if Input.is_action_pressed("Sprint"):
+			velocity.x = direction * (speed*2)
+			textureAnimation.scale.x = -direction
+			texture.scale.x = -direction
+			textureAnimation.play("Run")
+		else:
+			velocity.x = direction * speed
+			textureAnimation.play("Walk")
+			texture.scale.x = -direction
+			textureAnimation.scale.x = -direction
+	else:
+		velocity.x = move_toward(velocity.x,0,speed)
+		textureAnimation.play("Idle")
+		texture.scale.x = 1
+		textureAnimation.scale.x = -direction
+		
 	move_and_slide()
 	
